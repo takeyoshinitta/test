8abb5760a6ff280591851b52f457f4d4a2cb3e8d
Sheryl Giovanni Tania <sheryltania@Sheryls-MacBook-Air.local>
2022-01-31 18:17:43 -1000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(introduce("Sheryl"));
+        System.out.println("I am " + age() + ".");
+    }
+
+    private static String introduce(String name) {
+        return "Hi, my name is " + name + ".";
+    }
+
+    private static int age() {
+        return 19;
+    }
+
+    // Takeyoshi: make a method to return hobbies, put it in main
+    // Jason: make a method to return occupation, put it in main
+    // Naoshi: make a method to return nationality, put it in main
+
+
+}
