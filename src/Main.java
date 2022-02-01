5f849c311b7d0a3b22091a014b2bcb2d467be3fc
Takeyoshi Nitta <c6afc0c0b9d6bc@gmail.com>
2022-01-31 19:31:30 -1000

    Update main.java
    Add the method

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(introduce("Sheryl"));
         System.out.println("I am " + age() + ".");
+        System.out.println(hobbies("Sheryl"));
     }
 
     private static String introduce(String name) {
@@ -14,6 +15,14 @@ public class Main {
     }
 
     // Takeyoshi: make a method to return hobbies, put it in main
+    private static String hobbies(String s) {
+        if (s.equals("Takeyoshi")) {
+            return "My hobby is playing rugby";
+        }
+        else {
+            return "My hobby is coding";
+        }
+    }
     // Jason: make a method to return occupation, put it in main
     // Naoshi: make a method to return nationality, put it in main
 
